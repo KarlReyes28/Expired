1658ea36f5c3ce6dec475340d80cb0bcec2fceac
Karl Michael Reyes <109103579+KarlReyes28@users.noreply.github.com>
2023-02-09 12:25:31 -0500

    [#13] Apply suggestions from code review
    Co-authored-by: Donghua Shen <122644881+donghua-shen@users.noreply.github.com>

--- Expired/Views/SettingView.swift
+++ Expired/Views/SettingView.swift
@@ -15,36 +15,33 @@ struct SettingView: View {
     @State private var deleteSuccess: Bool = false
 
     var body: some View {
-        // Input here for Sandeep's part
+    NavigationView {
         List {
-            Section(header: Text("Archive List")) {
-                Text("test")
-            }
-
             Section(header: Text("Data Management")) {
                 Group {
-                    Button("Remove all products") {
+                    Button("Delete all data") {
                        showingDeleteAlert = true
                     }
-                    .alert("Are you sure you want to remove all products? \n \n Deleting it will not be recoverable.", isPresented: $showingDeleteAlert) {
-                        Button("No", role: .cancel) {
-                            
-                        }
+                    .foregroundColor(.red)
+                    .alert("Are you sure you want to delete all data?", isPresented: $showingDeleteAlert, actions: {
+                        Button("No", role: .cancel) { }
                         Button("Yes", role: .destructive, action: {
-                            showingSuccessAlert = productStore.deleteAll(viewContext)
+                            deleteSuccess = productStore.deleteAll(viewContext)
+                            showingDeleteResultAlert = true
                         })
-                    }
-                    .alert(
-                        "Successfully deleted all records.",
-                        isPresented: $showingSuccessAlert
-                    ) {
-                        Button("OK") {
-                            // Handle the acknowledgement.
+                    }, message: {
+                        Text("Deleting it will not be recoverable")
+                    })
+                    .alert(deleteSuccess ? "Successfully deleted all data" : "Deleting failure\nPlease try again", isPresented: $showingDeleteResultAlert) {
+                        Button("OK", role: .cancel) {
+                            deleteSuccess = false
                         }
                     }
                 }
             }
-        }.listStyle(.insetGrouped)
+        }
+            .navigationBarTitle("Settings")
+        }
     }
 }
 
