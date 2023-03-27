ea84b23dd67290f142c077cf103ce1e93ba6ec7b
Karl Michael M Reyes <kmreyes.can@gmail.com>
2023-03-27 19:11:25 -0400

    [#16] Implemented List Footer showing counts

---
+++ Expired/Views/ProductsFooterView.swift
@@ -0,0 +1,21 @@
+//
+//  ProductsFooterView.swift
+//  Expired
+//
+//  Created by Karl Michael Reyes on 2023-03-27.
+//
+
+import SwiftUI
+
+struct ProductsFooterView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductsFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductsFooterView()
+    }
+}
